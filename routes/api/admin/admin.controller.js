2119e7c05d48b50637c55be5ffac933481a0fe8d
JIH0LEE <skql5810@gmail.com>
2021-07-15 19:38:36 +0900

    유저 블락기능 완성

--- routes/api/admin/admin.controller.js
+++ routes/api/admin/admin.controller.js
@@ -6,3 +6,22 @@ exports.getUserList =async(req,res)=>{
         userList:userList
     })
 };
+
+
+exports.changeUserState =async(req,res)=>{
+    const id=req.body.userid
+    const status=req.body.userStatus
+    User.findByIdAndUpdate(id,{status: !status},(err,doc)=>{
+        if(err){
+            res.status(403).json({
+                success: false
+            })
+        }
+        else{
+            res.json({
+                status:!status
+            })
+        }
+    })
+ 
+};
--- routes/api/admin/index.js
+++ routes/api/admin/index.js
@@ -3,6 +3,6 @@ var router = express.Router();
 const controller = require('./admin.controller');
 
 router.get('/userlist',controller.getUserList);
-
+router.post('/statuschange',controller.changeUserState)
 
 module.exports = router;
