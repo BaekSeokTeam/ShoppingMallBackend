5f2e3d5773361ba15a1637dba820e6062e7e903f
JIH0LEE <skql5810@gmail.com>
2021-07-17 12:15:56 +0900

    multer package 추가 yarn install 하셈

--- routes/test.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* test-dk. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: '덕규바보2yyyyyyyyyyyyyyyyyyyyyaaaaa' });
-});
-
-module.exports = router;
